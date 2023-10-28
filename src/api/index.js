7eb9729cf7c8d77fdf328aaed4089244f598f1b7
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-28 15:43:47 +0300

    add func savedata

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,30 @@
 const BASE_URL = "http://localhost:3000/contacts/";
+// function saveData(data) {
+//   return fetch(BASE_URL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   }).then((res) => {
+//     if (!res.ok) {
+//       throw new Error(res.statusText || res.status);
+//     }
+//     return res.json();
+//   });
+// }
+
+async function saveData(data) {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText || response.status);
+  }
+  return response.json();
+}
+export { saveData };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { formEl } from "./refs";
+import { saveData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
 formEl.addEventListener("submit", handlerSubmit);
 
 function handlerSubmit(evt) {
-    evt.preventDefault();
-    const formData = new FormData(evt.target);
-    const data = Object.fromEntries(formData);
-    formEl.reset();
-  
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const data = Object.fromEntries(formData);
+  formEl.reset();
+  data.createdAt = Date.now();
+  saveData(data).then((ans) => console.log(ans));
 }
